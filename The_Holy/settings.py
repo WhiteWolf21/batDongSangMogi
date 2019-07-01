998e72e5301c83fa50213c68ef94c1d9f2b72e24
Hoang Le <hoangle1001@outlook.com>
2019-07-01 18:11:23 +0700

    Remove duplicated directory

--- The_Holy/settings.py
+++
@@ -1,9 +0,0 @@
-
-ACCT_FILE     = "acc.csv"
-URL           = "https://www.facebook.com/"
-CHROME_DRIVER = "drivers/chromedriver"
-GROUPS        = [
-    "mogivietnam",
-]
-
-SCROLLS       = 5
--- The_Holy/the_crawler.py
+++
@@ -1,9 +0,0 @@
-import settings
-from modules.crawl.sign_in import sign_in
-
-def main():
-    for page in settings.GROUPS:
-        sign_in(page)
-
-if __name__ == "__main__":
-    main()
