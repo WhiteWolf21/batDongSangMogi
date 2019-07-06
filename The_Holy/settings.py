99232a6a5f14a56b5eb499109b7fea3df48009cf
vux <1552437@hcmut.edu.vn>
2019-07-06 21:22:04 +0700

    update intent

--- The_Holy/settings.py
+++
@@ -1,7 +0,0 @@
-
-ACCT_FILE     = "acc.csv"
-URL           = "https://www.facebook.com/"
-CHROME_DRIVER = "drivers/chromedriver"
-GROUP         = "https://www.facebook.com/groups/mogivietnam/"
-
-SCROLLS       = 5
--- The_Holy/the_crawler.py
+++
@@ -1,7 +0,0 @@
-from modules.crawl.sign_in import sign_in
-
-def main():
-    sign_in()
-
-if __name__ == "__main__":
-    main()
