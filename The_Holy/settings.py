f954528e8bc5c056c70fc82569294aa9a5f32298
Huynh Ngoc Thien <gunshot1st@gmail.com>
2020-01-20 16:05:20 +0700

    Del
    redo

--- The_Holy/settings.py
+++
@@ -1,7 +0,0 @@
-
-ACCT_FILE     = "acc.csv"
-URL           = "https://www.facebook.com/"
-CHROME_DRIVER = "drivers/chromedriver.exe"
-GROUP         = "https://www.facebook.com/mogivietnam/"
-
-SCROLLS       = 50
--- WebApp/tag_service/scripts/run_service.sh
+++
@@ -1,7 +0,0 @@
-
-env PYTHONUNBUFFERED=true gunicorn \
-    --workers 2 \
-    --max-requests 10000 \
-    --timeout 300 \
-    --access-logfile - \
-    app:app -b 0.0.0.0:5000
